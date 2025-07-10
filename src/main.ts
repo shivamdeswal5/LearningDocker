7974ff9f4958deb3ff6ea0fb3bcf23626288f481
Ankit <ankitkumar2003b@gmail.com>
2025-07-10 18:11:21 +0530

    docker-backend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  });
+  await app.listen(process.env.APP_PORT ?? 3001);
 }
 bootstrap();
